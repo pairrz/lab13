2bbe7cb2c9ab419a49f515216f3ea14ee3931487
pairrz <dararat.buayim@gmail.com>
2024-01-02 16:29:54 +0700

    01

--- lab13_1.cpp
+++ lab13_1.cpp
@@ -17,3 +17,24 @@ int main(){
     cout << "\nMin = " << B[5];
     return 0;
 }
+
+void stat(const double a[],int n ,double b[]){
+    double sum1 = 0,sum2 = 1,sum3 = 0 , mn = 1e6,mx = 0,sd = 0;
+    for(int i = 0; i < n; i++){
+        if(a[i] > mx) mx = a[i];
+        if(a[i] < mn) mn = a[i];
+        sum1 += a[i];
+        sum2 *= a[i];
+        sum3 += 1/a[i];
+    }
+    b[0] = sum1 / n;
+    for(int i = 0; i < n; i++){
+        sd += pow((a[i] - b[0]),2);
+    }
+    b[1] = sqrt(sd/n);
+    b[2] = pow(sum2,1.0/n);
+    b[3] = n / sum3;
+    b[4] = mx;
+    b[5] = mn;
+    
+}
