467d7858558d0540237bfd12457fbdf0904d1a88
pairrz <dararat.buayim@gmail.com>
2024-01-02 16:30:26 +0700

    02

--- lab13_2.cpp
+++ lab13_2.cpp
@@ -21,3 +21,25 @@ int main()
     }while(s != 0 || x != 0 || y != 0);
     return 0;
 }
+
+void updateImage(bool img[N][M],int s,int x , int y){
+    for(int i = 0; i<70; i++){
+        for(int j = 0; j<70; j++){
+            if(sqrt(pow(x-i,2) + pow(y-j,2)) <= s-1) img[i][j] = 1;
+        }
+    }
+}
+
+void showImage(const bool img[N][M]){
+    cout <<"------------------------------------------------------------------------\n";
+    for(int i = 0; i<30; i++){
+        for(int j = 0; j<70; j++){
+            if(j == 0) cout << "|";
+            if(img[i][j] == 0)cout << " ";
+            else cout << "*";
+            if(j == 69) cout << "|";
+        }
+        cout << endl;
+    }
+    cout <<"------------------------------------------------------------------------\n";
+}
